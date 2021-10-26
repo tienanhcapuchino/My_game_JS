59143028fcff66279e2535359b13d877516ef46f
tienanhcapuchino <88329721+tienanhcapuchino@users.noreply.github.com>
2021-10-26 20:04:48 +0700

    update

--- game.js
+++ game.js
@@ -1,10 +1,19 @@
-window.onload = function() {
-    var game = new Phaser.game();
-}
+
+
 var config = {
     width: 256,
     height: 272,
     backgroundColor: 0x000000,
-    scene: [Scene1, Scene2]
-}
-var game = new Phaser.Game(config);
+    scene: [Scene1, Scene2],
+    pixelArt: true,
+    // 1.1 set the physics to arcade
+    physics: {
+      default: "arcade",
+      arcade:{
+          debug: false
+      }
+    }
+  }
+  
+  var game = new Phaser.Game(config);
+  
